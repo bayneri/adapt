3743d33fcafa0f7acf9aa1011f80c170421796d9
cetinerhalil <hhalilcetiner@gmail.com>
2018-12-01 00:59:04 +0300

    platforms.js created

---
+++ platforms.js
@@ -0,0 +1,21 @@
+module.exports = {
+    shortened: ['be/', 'bl/', 'dc/', 'dr/', 'fb/', 'gh/', 'in/', 'li/', 'md/', 'pt/', 'sk/', 'sp/', 'tg/', 'tw/', 'vi/', 'yt/'],
+    icons: [
+        'behance',
+        'blogger',
+        'discord',
+        'dribbble',
+        'facebook',
+        'github',
+        'instagram',
+        'linkedin',
+        'medium',
+        'pinterest',
+        'skype',
+        'spotify',
+        'telegram',
+        'twitter',
+        'vimeo',
+        'youtube'
+    ]
+};
